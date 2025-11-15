bf741b3084cf63f3789d89e6b81444e12e48ce1e
Zebenay Bitew <zebenay.bitew@bristol.ac.uk>
2025-11-15 11:40:14 -0600

    newscript

---
+++ R_scripts/enrichment.R
@@ -0,0 +1,182 @@
+###########################################################
+# Script: get_ensembl_ids.R
+# Purpose: Retrieve Ensembl Gene IDs + run enrichment analysis
+###########################################################
+
+library(biomaRt)
+library(data.table)
+library(dplyr)
+library(clusterProfiler)
+library(org.Hs.eg.db)
+library(ReactomePA)
+
+setwd("~/Library/CloudStorage/OneDrive-UniversityofBristol/CSHL")
+
+###########################################################
+### 1) Load files
+###########################################################
+
+df <- fread("/Users/cr23646/Desktop/background genes.txt")
+
+prot_291 <- fread(
+  "/Users/cr23646/Desktop/CRC/chap_1/11_final_291_proteins.txt",
+  sep = "\t", quote = FALSE, header = FALSE
+)
+
+prot_subset <- prot_291 %>%
+  select(UniProt = V2, OlinkID = V3, ProteinName = V4) %>%
+  mutate(across(everything(), ~ gsub('"', "", .x)))
+
+###########################################################
+### 2) Query Ensembl
+###########################################################
+
+ensembl <- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+
+results_uniprot <- getBM(
+  attributes = c("uniprotswissprot", "ensembl_gene_id", "hgnc_symbol"),
+  filters = "uniprotswissprot",
+  values = df$UniProt,
+  mart = ensembl
+)
+
+merged_data <- merge(
+  df, results_uniprot,
+  by.x = "UniProt", by.y = "uniprotswissprot",
+  all.x = TRUE
+)
+
+fwrite(merged_data, "ensembl_ids_mapped.txt", sep = "\t", quote = FALSE)
+
+df3 <- fread("ensembl_ids_mapped.txt")
+
+merged_data <- prot_subset %>% left_join(df3, by = c("UniProt", "OlinkID"))
+
+###########################################################
+### 3) Background + significant lists
+###########################################################
+
+Olink_proteins <- df3 %>% select(ensembl_gene_id)
+sig_proteins  <- merged_data %>% select(ensembl_gene_id)
+
+fwrite(Olink_proteins, "Olink_proteins.txt", col.names = FALSE, quote = FALSE)
+fwrite(sig_proteins,  "sig_proteins.txt",  col.names = FALSE, quote = FALSE)
+
+###########################################################
+### 4) Convert to ENTREZ
+###########################################################
+
+sig_ids <- bitr(sig_proteins$ensembl_gene_id, fromType="ENSEMBL",
+                toType="ENTREZID", OrgDb="org.Hs.eg.db")
+
+bg_ids  <- bitr(Olink_proteins$ensembl_gene_id, fromType="ENSEMBL",
+                toType="ENTREZID", OrgDb="org.Hs.eg.db")
+
+###########################################################
+### 5) Helper functions (NO repetition)
+###########################################################
+
+create_outdir <- function(path) dir.create(path, recursive = TRUE, showWarnings = FALSE)
+
+safe_plot <- function(p, file, out) {
+  if (is.null(p)) return(NULL)
+  png(file.path(out, paste0(file, ".png")), 1200, 1000, res = 150)
+  print(p)
+  dev.off()
+}
+
+safe_table <- function(x, name, out) {
+  df <- try(as.data.frame(x), silent = TRUE)
+  if (!inherits(df, "try-error") && nrow(df) > 0) {
+    write.csv(df, file.path(out, paste0(name, ".csv")), row.names = FALSE)
+  }
+}
+
+###########################################################
+### 6) General Enrichment Function
+###########################################################
+
+run_enrichment <- function(entrez, bg, outdir, label, show_n = 20) {
+  
+  create_outdir(outdir)
+  
+  go_onts <- c("BP", "MF", "CC")
+  
+  go_res <- lapply(go_onts, function(ont) {
+    enrichGO(gene = entrez,
+             OrgDb = org.Hs.eg.db,
+             keyType = "ENTREZID",
+             ont = ont,
+             pAdjustMethod = "BH",
+             pvalueCutoff = 0.05,
+             qvalueCutoff = 0.2,
+             universe = bg,
+             readable = TRUE)
+  })
+  names(go_res) <- paste0("GO_", go_onts)
+  
+  ekegg <- enrichKEGG(gene = entrez, organism = "hsa",
+                      keyType = "ncbi-geneid",
+                      pvalueCutoff = 0.05, qvalueCutoff = 0.2,
+                      universe = bg)
+  
+  er <- enrichPathway(gene = entrez, universe = bg,
+                      pAdjustMethod = "BH", qvalueCutoff = 0.05)
+  
+  # Save tables
+  lapply(names(go_res), function(n)
+    safe_table(go_res[[n]], paste0(label, "_", n), outdir))
+  
+  safe_table(ekegg, paste0(label, "_KEGG"), outdir)
+  safe_table(er,    paste0(label, "_Reactome"), outdir)
+  
+  # Plots
+  plotlist <- list(
+    GO_BP = barplot(go_res$GO_BP, showCategory = show_n) + theme_minimal(),
+    GO_MF = barplot(go_res$GO_MF, showCategory = show_n) + theme_minimal(),
+    GO_CC = barplot(go_res$GO_CC, showCategory = show_n) + theme_minimal(),
+    KEGG  = dotplot(ekegg, showCategory = show_n) + theme_minimal(),
+    Reactome = dotplot(er, showCategory = show_n) + theme_minimal()
+  )
+  
+  lapply(names(plotlist), function(n)
+    safe_plot(plotlist[[n]], paste0(label, "_", n), outdir))
+  
+  return(list(GO = go_res, KEGG = ekegg, Reactome = er))
+}
+
+###########################################################
+### 7) ENRICHMENT RUNS (ONE BLOCK ONLY)
+###########################################################
+
+results_all <- run_enrichment(sig_ids$ENTREZID, bg_ids$ENTREZID,
+                              outdir = "enrichment_results/all",
+                              label = "ALL")
+
+###########################################################
+### 8) Direction-specific enrichment
+###########################################################
+
+protein_df <- fread("/Users/cr23646/Desktop/CRC/chap_1/mapping/gene_mapping/mapping_GRCh38.p14_olink.txt")
+protein_df <- merge(protein_df, prot_subset, by.x = "Target", by.y = "ProteinName")
+
+protein_df <- protein_df[!duplicated(protein_df$OlinkID)]
+
+protein_up   <- protein_df[regulation == "Upregulated"]
+protein_down <- protein_df[regulation == "Downregulated"]
+
+# Run UP
+results_up <- run_enrichment(
+  protein_up$entrezgene_id,
+  bg_ids$ENTREZID,
+  outdir = "enrichment_results/up",
+  label = "UP"
+)
+
+# Run DOWN
+results_down <- run_enrichment(
+  protein_down$entrezgene_id,
+  bg_ids$ENTREZID,
+  outdir = "enrichment_results/down",
+  label = "DOWN"
+)
